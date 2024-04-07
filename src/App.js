6baedd4fa8ccbb304156eab2b110410f642c47ba
bobbyhadz <opmftw@gmail.com>
2024-04-07 15:41:35 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+import {useState} from 'react';
+
+const App = () => {
+  const options = [
+    {value: '', text: '--Choose an option--'},
+    {value: 'apple', text: 'Apple 🍏'},
+    {value: 'banana', text: 'Banana 🍌'},
+    {value: 'kiwi', text: 'Kiwi 🥝'},
+  ];
+
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleChange = event => {
+    console.log(event.target.value);
+    setSelected(event.target.value);
+  };
+
+  return (
+    <div>
+      <select value={selected} onChange={handleChange}>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default App;
